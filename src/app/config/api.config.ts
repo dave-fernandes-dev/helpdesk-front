31f19d5555a84db0562dfe04c1f0e79e0662f4e5
Dave Fernandes <dave.analista@gmail.com>
2021-11-17 17:06:09 -0300

    corrigido imports, api_config, e styles.scss

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  // baseUrl : 'https://helpdesk-prod.herokuapp.com'
-   baseUrl : 'localhost:4200'
+   baseUrl : 'https://vl-helpdesk-api.herokuapp.com'
+  // baseUrl : 'localhost:8080'
 }
