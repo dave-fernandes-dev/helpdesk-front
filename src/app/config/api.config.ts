628eb13217d6bd00290cb37d46c2f62251e8ca33
Dave Fernandes <83511244+dave-fernandes-dev@users.noreply.github.com>
2024-01-17 12:51:52 -0300

    Update api.config.ts   railway

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-  baseUrl : 'https://vl-helpdesk-api.herokuapp.com'
+  //baseUrl : 'https://vl-helpdesk-api.herokuapp.com'
+  baseUrl : 'https://helpdesk-api-vl.up.railway.app'  
   //baseUrl : 'http://localhost:8080'
 }
